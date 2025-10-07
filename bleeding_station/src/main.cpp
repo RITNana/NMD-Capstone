1d1912ae514189b836ced44db0875a465c81ebe7
Nana Sarpong <ns8363@g.rit.edu>
2025-10-07 15:29:30 -0400

    prototype done. need more visuals

--- bleeding_station/src/main.cpp
+++ bleeding_station/src/main.cpp
@@ -6,7 +6,7 @@ const int ledPins[] = {2, 3, 4};
 const int buttonPin = 7;
 
 // Light threshold value
-const int lightThreshold = 300;
+const int lightThreshold = 100;
 
 // LED state flag
 bool ledsOn = false;
@@ -20,6 +20,7 @@ const int delayBetweenLEDs = 1000;
 // value for buttonHold
 int chargeNum = 0;
 
+// lig
 
 void setup()
 {
@@ -36,24 +37,33 @@ void setup()
   Serial.begin(9600);
 }
 
-void loop() {
+void loop()
+{
   int lightLevel = analogRead(photoresistorPin);
   bool pressed = (digitalRead(buttonPin) == LOW); // INPUT_PULLUP: LOW = pressed
 
   // use only ONE of these definitions for bothTrueStreak (either global OR static)
   static uint8_t bothTrueStreak = 0;
 
-  if (lightLevel > lightThreshold && pressed) {
-    if (bothTrueStreak < 3) bothTrueStreak++;
-  } else {
+  if (lightLevel > lightThreshold && pressed)
+  {
+    if (bothTrueStreak < 3)
+      bothTrueStreak++;
+  }
+  else
+  {
     bothTrueStreak = 0;
   }
 
-  if (bothTrueStreak >= 3) {
+  if (bothTrueStreak >= 3)
+  {
     chargeNum++;
     bothTrueStreak = 0;
-  } else if (!pressed || lightLevel <= lightThreshold) {
-    if (chargeNum > 0) chargeNum--;
+  }
+  else if (!pressed || lightLevel <= lightThreshold)
+  {
+    if (chargeNum > 0)
+      chargeNum--;
   }
 
   digitalWrite(ledPins[0], chargeNum > 10 ? HIGH : LOW);
@@ -65,4 +75,3 @@ void loop() {
 
   delay(100);
 }
-
--- src/index.js
+++ src/index.js
@@ -40,14 +40,14 @@ const connect_to_serial = async () => {
   try {
     port = new SerialPort({
       //change com port as needed
-      path: "COM3",
+      path: "COM4",
       baudRate: 9600,
     });
     const parser = port.pipe(new ReadlineParser({ delimiter: "\n" }));
 
     port.on("open", () => {
       //change com port as needed
-      console.log("Serial Port Opened on COM3 @ 9600");
+      console.log("Serial Port Opened on COM4 @ 9600");
     });
 
     port.on("error", (err) => {
