110b3569f8574e1f5ff25008c0f1becd1332ec33
ForSchoolJackson <jmh4687@g.rit.edu>
2025-10-29 21:02:58 -0400

    popout function + needs parsing

--- tummy_station/src/main.cpp
+++ tummy_station/src/main.cpp
@@ -19,9 +19,9 @@ const int lightThreshold = 33;
 
 int averageLight;
 
-//output for wifi
-bool input;
-int output = 0;
+// output for wifi
+int output = 1;
+String go = "false";
 
 int calibrate()
 {
@@ -66,6 +66,25 @@ void tummySetup()
   calibrate();
 }
 
+void popout()
+{
+
+  if (Serial.available() > 0)
+  {
+    char key = Serial.read();
+  }
+  if (go == "true")
+  {
+    Serial.println("opening");
+    servoR.write(0);
+    servoL.write(190);
+    delay(500);
+
+    output = 0; // open
+    go = "false";
+  }
+}
+
 // change to tummyLoop()
 int tummyLoop()
 {
@@ -94,7 +113,7 @@ int tummyLoop()
       servoL.write(190);
       delay(500);
 
-      output = 90; //open
+      output = 0; // open
     }
     else if (key == 'l' || key == 'L')
     {
@@ -103,7 +122,7 @@ int tummyLoop()
       servoL.write(angleL);
       delay(500);
 
-      output = 0; //closed
+      output = 1; // closed
     }
   }
 
@@ -114,12 +133,12 @@ int tummyLoop()
     servoL.write(angleL);
     Serial.println("closing");
 
-    output = 0; //closed
+    output = 1; // closed
   }
 
-  //Serial.println(connectionLight);
-  //Serial.println(output);
-  //output = servoR.read();
+  // Serial.println(connectionLight);
+  // Serial.println(output);
+  // output = servoR.read();
 
   return output;
 }
@@ -127,9 +146,9 @@ int tummyLoop()
 // // -----------------------------------------------------
 // // WIFI SETUP
 // // -----------------------------------------------------
-char ssid[] = "F00KKA9";        // your network SSID (name)
-char pass[] = "PleaseWork";    // your network password (use for WPA, or use as key for WEP)
-int keyIndex = 0;            // your network key index number (needed only for WEP)
+char ssid[] = "F00KKA9";    // your network SSID (name)
+char pass[] = "PleaseWork"; // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;           // your network key index number (needed only for WEP)
 
 int status = WL_IDLE_STATUS;
 
@@ -139,45 +158,48 @@ WiFiClient client;
 // server address:
 char server[] = "192.168.137.1";
 // char example[] = "1";
-//IPAddress server(64,131,82,241);
+// IPAddress server(64,131,82,241);
 
-unsigned long lastConnectionTime = 0;            // last time you connected to the server, in milliseconds
+unsigned long lastConnectionTime = 0;              // last time you connected to the server, in milliseconds
 const unsigned long postingInterval = 10L * 1000L; // delay between updates, in milliseconds
 
 /* just wrap the received data up to 80 columns in the serial print*/
 /* -------------------------------------------------------------------------- */
-void read_request() {
-/* -------------------------------------------------------------------------- */
+void read_request()
+{
+  /* -------------------------------------------------------------------------- */
   uint32_t received_data_num = 0;
 
-  while (client.available()) {
+  while (client.available())
+  {
     /* actual data reception */
     char c = client.read();
     /* print data to serial port */
     Serial.print(c);
     /* wrap data to 80 columns*/
     received_data_num++;
-    if(received_data_num % 80 == 0) {
-
+    if (received_data_num % 80 == 0)
+    {
     }
-
   }
 }
 
 // this method makes a HTTP connection to the server:
 /* -------------------------------------------------------------------------- */
-void httpRequest(int data) {
-/* -------------------------------------------------------------------------- */
+void httpRequest(int data)
+{
+  /* -------------------------------------------------------------------------- */
   // close any connection before send a new request.
   // This will free the socket on the NINA module
   client.stop();
 
   // if there's a successful connection:
-  if (client.connect(server, 3000)) { //Server address from above & Port
+  if (client.connect(server, 3000))
+  { // Server address from above & Port
     // Serial.println("connecting..."); //Really here for logging
     // send the HTTP GET request:
-    client.println("GET /tummy HTTP/1.1"); //GET request at '/' using HTTP/1.1
-    client.println("Host: Tummy"); //Required but the input doesnt matter
+    client.println("GET /tummy HTTP/1.1"); // GET request at '/' using HTTP/1.1
+    client.println("Host: Tummy");         // Required but the input doesnt matter
     client.print("Data:");
     client.println(data);
     // client.println("User-Agent: ArduinoWiFi/1.1"); //Not required
@@ -185,15 +207,18 @@ void httpRequest(int data) {
     client.println();
     // note the time that the connection was made:
     lastConnectionTime = millis();
-  } else {
+  }
+  else
+  {
     // if you couldn't make a connection:
     Serial.println("connection failed");
   }
 }
 
 /* -------------------------------------------------------------------------- */
-void printWifiStatus() {
-/* -------------------------------------------------------------------------- */
+void printWifiStatus()
+{
+  /* -------------------------------------------------------------------------- */
   // print the SSID of the network you're attached to:
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
@@ -211,44 +236,51 @@ void printWifiStatus() {
 }
 
 /* -------------------------------------------------------------------------- */
-void setup() {
-/* -------------------------------------------------------------------------- */
-  //Initialize serial and wait for port to open:
+void setup()
+{
+  /* -------------------------------------------------------------------------- */
+  // Initialize serial and wait for port to open:
   Serial.begin(9600);
   tummySetup();
-  while (!Serial) {
+  while (!Serial)
+  {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
   // check for the WiFi module:
-  if (WiFi.status() == WL_NO_MODULE) {
+  if (WiFi.status() == WL_NO_MODULE)
+  {
     Serial.println("Communication with WiFi module failed!");
     // don't continue
-    while (true);
+    while (true)
+      ;
   }
 
   String fv = WiFi.firmwareVersion();
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION)
+  {
     Serial.println("Please upgrade the firmware");
   }
 
   // attempt to connect to WiFi network:
-  while (status != WL_CONNECTED) {
+  while (status != WL_CONNECTED)
+  {
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(ssid);
     // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
     status = WiFi.begin(ssid, pass);
 
     // wait 10 seconds for connection:
-    //delay(10000);
+    // delay(10000);
   }
   // you're connected now, so print out the status:
   printWifiStatus();
 }
 
 /* -------------------------------------------------------------------------- */
-void loop() {
-/* -------------------------------------------------------------------------- */
+void loop()
+{
+  /* -------------------------------------------------------------------------- */
   // if there's incoming data from the net connection.
   // send it out the serial port.  This is for debugging
   // purposes only:
@@ -262,7 +294,6 @@ void loop() {
   int valuedata = tummyLoop();
   httpRequest(valuedata);
 }
-
 
 // // -----------------------------------------------------
 // // COM TESTING
